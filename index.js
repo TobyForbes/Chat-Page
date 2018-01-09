936ef82ecc3bd9aa4b505161654ecdd92a998e4c
Toby Forbes <toby.forbes2002@gmail.com>
2018-01-09 17:35:25 +0100

    Added documentation to index.js

--- index.js
+++ index.js
@@ -7,15 +7,11 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
-/**io.on('connection', function(socket){
-	console.log('a user connected');
-});*/
-
 /* Sends a message to the console when a user has connected or distontected from the server (acsesed the webpage) */
-io.on('connection', function(socket){
-	console.log('a user connected');
-	socket.on('disconnect', function(){
-		console.log('user disconnect'); 
+io.on('connection', function(socket){ /* Asks itself whether someone has connected */
+	console.log('a user connected'); /* Outputs "a user connected" to the console when someone visits the webpage */
+	socket.on('disconnect', function(){ /* Asks itself whether someone has disconeccted */
+		console.log('a user disconnected'); /* Outputs "a user disconnected" to the console when someone closes the webpage */
 	});
 });
 
@@ -23,13 +19,12 @@ var timeStamp = Math.floor(Date.now());
 
 io.on('connection', function(socket){
 	socket.on('chat message', function(msg){
-		console.log(msg.pseudo + " " + msg.message + timeStamp);
+		console.log(msg.pseudo + " " + msg.message + " " + timeStamp); /* adds the username, message and timestamp together with spaces between and outputs it to the console */
 		io.emit('chat message', msg);
-		//console.log('message: ' + msg);
 	});
 });
 
-http.listen(3000, function(){
+http.listen(3000, function(){ 
 	console.log('listening on *:3000'); /* Sends message to the console when the server has sucsessfully been started using a node js terminal*/ 
 });
 
